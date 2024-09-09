8d7e5a4b3b80992e2137582c248e7655fbd58124
tvh0021 <trungha@my.unt.edu>
2024-09-09 17:01:24 -0400

    update basic structure of the aweSOM package

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Read the requirements from the requirements.txt file
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="aweSOM",
+    version="0.0.1",
+    author="Trung Ha",
+    author_email="tvha@umass.edu",
+    description="Accelerated Self-Organizing Maps and Statistically Combined Ensemble method for plasma intermittency detection",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/tvh0021/aweSOM.git",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=required,
+    include_package_data=True,
+)
