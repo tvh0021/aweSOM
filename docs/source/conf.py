4534532b83a7fd013c17e60768e0050edc5d6a91
tvh0021 <trungha@my.unt.edu>
2024-09-18 01:48:12 -0400

    update plasma example

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
+
 html_theme = "sphinx_rtd_theme"
-# html_theme_path = ['_themes']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_static_path = ["_static"]
 
 # -- Import path -------------------------------------------------------------
